18744aeab007370f4a0414e04796e993af320dda
mr-mmmmore <mr@mmmmore.com>
2018-12-24 11:26:56 +0100

    Prevent "mailto" urls replacement by tracking code

--- src/MailTracker.php
+++ src/MailTracker.php
@@ -83,7 +83,7 @@ class MailTracker implements \Swift_Events_SendListener
         $this->hash = $hash;
 
         $html = preg_replace_callback(
-            "/(<a[^>]*href=['\"])([^'\"]*)/",
+            "/(<a[^>]*href=['\"])(?!mailto)([^'\"]*)/",
                 [$this, 'inject_link_callback'],
                 $html
         );
